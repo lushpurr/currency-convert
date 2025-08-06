21fbd978359c0c1db1ebd1be7b607d4c9f3434e7
Gavin Will <gavinjwill@gmail.com>
2025-08-06 13:53:48 +0100

    Secured API key using Vercel and changed to use custom API

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -7,8 +7,8 @@ import { catchError, Observable, throwError } from 'rxjs';
 })
 export class DataService {
 
-  private apiKey = 'DAIQO2SUmcEW4Zv0FBllkgFz4oLYY55Z';
-  private apiUrl = `https://api.currencybeacon.com/v1/`;
+  // To ensure security of API Key I created custom API endpoints on Vercel
+  private apiUrl="https://my-currency-api-snowy.vercel.app/api/"
 
 
   constructor(
@@ -22,20 +22,17 @@ export class DataService {
     const currencyType = 'flat';
 
     return this.http
-      .get(`${this.apiUrl}currencies?type=${currencyType}&api_key=${this.apiKey}`)
+      .get(`${this.apiUrl}currencies?type=${currencyType}`)
       .pipe(catchError(this.handleError));
   
   }
 
   calculateCurrencyValue(fromCurrency: string, toCurrency: string, amount: number): Observable<any>{
     // https://api.currencybeacon.com/v1/convert
-    //     from - Required - The base currency you would like to convert from.
-    // to - Required - The currency you would like to convert to.
-    // amount - Required - The amount to convert.
     console.log('currency convert input', fromCurrency, toCurrency, amount)
 
     return this.http
-      .get(`${this.apiUrl}convert?from=${fromCurrency}&to=${toCurrency}&amount=${amount}&api_key=${this.apiKey}`)
+      .get(`${this.apiUrl}convert?from=${fromCurrency}&to=${toCurrency}&amount=${amount}`)
       .pipe(catchError(this.handleError));
   }
 
