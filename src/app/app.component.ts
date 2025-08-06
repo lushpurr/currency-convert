97a4eb63c8bf5c95d96f739e293071f0fb473d46
Gavin Will <gavinjwill@gmail.com>
2025-08-06 12:00:56 +0100

    Change app to standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SharedImports } from './shared/shared-imports';
+import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [SharedImports, MainComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'currency-converter';
 }
-
