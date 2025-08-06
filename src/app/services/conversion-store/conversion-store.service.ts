cd387fa445b661b1d34a613421963b4ab614efe0
Gavin Will <gavinjwill@gmail.com>
2025-08-06 22:17:37 +0100

    Simplify template using computed

--- src/app/services/conversion-store/conversion-store.service.ts
+++ src/app/services/conversion-store/conversion-store.service.ts
@@ -9,9 +9,29 @@ export class ConversionStore {
   selectedFromCurrency = signal<CurrencyObject | null>(null);
   selectedToCurrency = signal<CurrencyObject | null>(null);
 
-  result = computed(() =>
-    this.toValue() ? `${this.fromValue()} converts to ${this.toValue()}` : ''
-  );
+  // result = computed(() =>
+  //   this.toValue() ? `${this.fromValue()} converts to ${this.toValue()}` : ''
+  // );
+
+    // Computed signals for template
+  fromDisplay = computed(() => {
+    const currency = this.selectedFromCurrency();
+    const value = this.fromValue();
+    return currency ? { value, code: currency.short_code, name: currency.name } : null;
+  });
+
+  toDisplay = computed(() => {
+    const currency = this.selectedToCurrency();
+    const value = this.toValue();
+    return currency ? { value, code: currency.short_code, name: currency.name } : null;
+  });
+
+  conversionText = computed(() => {
+    const from = this.fromDisplay();
+    const to = this.toDisplay();
+    if (!from || !to) return '';
+    return `${from.value} ${from.name} equals`;
+  });
 
   setFromValue(value: number) {
     this.fromValue.set(value);
